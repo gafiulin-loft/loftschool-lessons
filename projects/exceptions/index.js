2e8558ecd01ea6b9992a52e07faad23e274cc9d3
Timur Gafiulin <timur.gafiulin@creativpeople.agency>
2020-10-20 21:30:36 +0300

    homework_3 done

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -16,7 +16,21 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
-function isAllTrue(array, fn) {}
+function isAllTrue(array, fn) {
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array');
+  } else if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (!fn(array[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 /*
  Задание 2:
@@ -34,7 +48,21 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array');
+  } else if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (fn(array[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 /*
  Задание 3:
@@ -47,7 +75,23 @@ function isSomeTrue(array, fn) {}
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn, ...args) {}
+function returnBadArguments(fn, ...args) {
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  const array = new Array();
+
+  for (let i = 0; i < args.length; i++) {
+    try {
+      fn(args[i]);
+    } catch {
+      array.push(args[i]);
+    }
+  }
+
+  return array;
+}
 
 /*
  Задание 4:
@@ -66,7 +110,55 @@ function returnBadArguments(fn, ...args) {}
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator(number = 0) {}
+function calculator(number = 0) {
+  if (!isFinite(number)) {
+    throw new Error('number is not a number');
+  }
+
+  const obj = {
+    sum: function () {
+      let result = number;
+
+      for (let i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+      }
+
+      return result;
+    },
+    dif: function () {
+      let result = number;
+
+      for (let i = 0; i < arguments.length; i++) {
+        result -= arguments[i];
+      }
+
+      return result;
+    },
+    div: function () {
+      let result = number;
+
+      for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] === 0) {
+          throw new Error('division by 0');
+        }
+        result /= arguments[i];
+      }
+
+      return result;
+    },
+    mul: function () {
+      let result = number;
+
+      for (let i = 0; i < arguments.length; i++) {
+        result *= arguments[i];
+      }
+
+      return result;
+    },
+  };
+
+  return obj;
+}
 
 /* При решении задач, постарайтесь использовать отладчик */
 
